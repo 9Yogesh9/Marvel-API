22bf02c99259384e064e2933560118201ceea8ae
Yogesh Bhat <yogesh99bhat@gmail.com>
2022-12-15 00:10:40 +0530

    Cards developed, Search functionality built, Handled no results on search

--- script.js
+++ script.js
@@ -1,22 +1,12 @@
-// Keys to be attached to get the response from Marvel server to every request
+// Keys to be attached to get the response from Marvel server to every request and different constant values
 const keys = "ts=1670913383902&apikey=edc9531ea872c74a2855ed93a5903229&hash=bbb581dcf34e4752243b361daa960fb1";
-const limit = "limit=100&";
+const limit10 = "limit=10&";
+const notfav_icon = "./assests/heart-unselected.svg";
+const fav_icon = "./assests/heart-selected.svg";
 
 // Different URL and there variables to be used
 let getCharacters = "http://gateway.marvel.com/v1/public/characters?";
-let getFilteredCharacters = "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=spider&";
-
-// const URL = "http://gateway.marvel.com/v1/public/comics?ts=1670913383902&apikey=edc9531ea872c74a2855ed93a5903229&hash=bbb581dcf34e4752243b361daa960fb1";
-// const URL = "http://gateway.marvel.com/v1/public/characters?ts=1670913383902&apikey=edc9531ea872c74a2855ed93a5903229&hash=bbb581dcf34e4752243b361daa960fb1";
-
-// get the Fetches the list of characters
-// https://gateway.marvel.com:443/v1/public/characters?apikey=edc9531ea872c74a2855ed93a5903229
-
-let getbtnR = document.getElementById("toclick");
-getbtnR.setAttribute('onclick', `fetchMaster('${getFilteredCharacters}${keys}')`)
-// let getbtn = document.getElementById("getbtn_link");
-// getbtn.setAttribute('href', URL);
-// getbtn.setAttribute('href', getFilteredCharacters + keys);
+let getFilteredCharacters = "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=";
 
 let listHolder = document.getElementById("listHolder");
 
@@ -24,6 +14,68 @@ let listHolder = document.getElementById("listHolder");
 function listAppend(inside_list) {
     listHolder.appendChild(inside_list);
 }
+
+function fetchMaster(URL = (getCharacters + limit10 + keys)) {
+    console.log("fetchMaster intiated !", URL);
+    fetch(URL)
+        .then((response) => response.json())
+        .then((data) => print_characters(data));
+}
+
+// to run onload 
+fetchMaster();
+// let holder = "";
+
+function print_characters(fetch_characters) {
+
+    let results = fetch_characters.data.results;
+    // holder = fetch_characters;
+    if (results.length != 0) {
+        listHolder.innerHTML = "";
+        for (r of results) {
+            let li_item = document.createElement('div');
+
+            let character_thumb = `<div class="character_thumb_container"><img src="${r.thumbnail.path}.${r.thumbnail.extension}" class="character_thumb" alt="" srcset=""></div>`
+            let character_name = `<div class="label_container"><p class="character_name">${r.name}</p>`;
+            let fav_button = `<div class="fav_thumb"><img src="${notfav_icon}" alt="" srcset="" id="img_${r.id}" onclick="addToFav(${r.id})">
+        </div></div>`
+
+            let character_div = `<div class="individual_character"> 
+        ${character_thumb}
+        ${character_name}
+        ${fav_button}
+        </div>`;
+
+            li_item.innerHTML = character_div;
+
+            // console.log(`${r.thumbnail.path}.${r.thumbnail.extension}`);
+
+            listAppend(li_item);
+        }
+    } else {
+        listHolder.innerHTML = "<h1> No Results Found ! Please try other keywords.";
+    }
+}
+
+let search_text = document.getElementById('search_text');
+
+function show_results() {
+    let search_value = search_text.value;
+    // console.log(`${getFilteredCharacters}${search_value}&${keys}`);
+    if (search_value)
+        fetchMaster(`${getFilteredCharacters}${search_value}&${keys}`);
+    else
+        alert("Please Enter a valid value !");
+}
+
+// Will be adding characters to the fav array
+function addToFav(charac_id) {
+    console.log(charac_id);
+    let ch_img = document.getElementById(`img_${charac_id}`);
+    ch_img.setAttribute("src", `${fav_icon}`);
+}
+
+
 
 // Gives you json data fetched from marvel server
 // function marvelRequestor(URL){
@@ -39,29 +91,3 @@ function listAppend(inside_list) {
 //     xhr.send();
 
 // }
-
-function fetchMaster(URL = getCharacters + keys) {
-    console.log("fetchMaster intiated !",URL);
-    fetch(URL)
-        .then((response) => response.json())
-        .then((data) => print_characters(data));
-}
-
-// to run onload 
-fetchMaster();
-
-function print_characters(fetch_characters) {
-
-    listHolder.innerHTML = "";
-    let results = fetch_characters.data.results;
-    for (r of results) {
-        let li_item = document.createElement('li');
-
-        li_item.innerHTML = `
-            <h2>${r.name}</h2>
-        `;
-
-        listAppend(li_item);
-    }
-
-}
